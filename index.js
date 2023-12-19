e73775086a5cb04db1948028a7d1e73edf61e06b
HimanshuGitCode <singhlink4@gmail.com>
2023-12-19 12:18:37 +0530

    area of circle

--- index.js
+++ index.js
@@ -11,19 +11,34 @@
 // }
 // x();
 
+//set time out using for loops
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(function cb(){
-  console.log("Call Back");
-},5000);
+// setTimeout(function cb(){
+//   console.log("Call Back");
+// },5000);
 
-console.log("End");
+// console.log("End");
 
-let startDate = new Date().getTime();
-let endDate = startDate;
-while(endDate < startDate + 10000){
- endDate = new Date().getTime();
-}
+// let startDate = new Date().getTime();
+// let endDate = startDate;
+// while(endDate < startDate + 10000){
+//  endDate = new Date().getTime();
+// }
 
-console.log("While Expire here"); 
+// console.log("While Expire here"); 
+
+// Higher order fucntion for calcualte area of circle
+
+const radius = [3,1,2,4];
+
+const calcualteArea = function(radius){
+  const output = [];
+  for(let i = 0;i<radius.lenght; i++){
+    output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+
+console.log(calcualteArea(radius));
